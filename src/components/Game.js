075ba3555a3f9315aeb21b65e3ffac21eb173345
Aaron Rodriguez <spoogemonkey199877@yahoo.com>
2020-02-03 17:48:49 -0600

    20

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,93 +1,90 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Question from './Question';
 import { loadQuestions } from '../helpers/QuestionsHelper';
 import HUD from './HUD';
 import SaveScoreForm from './SaveScoreForm';
 
-export default class Game extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      questions: null,
-      currentQuestion: null,
-      loading: true,
-      score: 0,
-      questionNumber: 0,
-      done: false,
-    };
-  }
+export default function Game({ history }) {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [score, setScore] = useState(0);
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [done, setDone] = useState(false);
   // retrieve question set from API using the Fetch API
   // convert that response to a format that we want
   // choose the first question of that array and pass it to the question component
 
-  async componentDidMount() {
-    try {
-      const questions = await loadQuestions();
-      console.log(questions);
-      this.setState({ questions }, () => {
-        this.changeQuestion();
-      },
-      );
-    } catch (err) {
-      console.log(err);
-    }
-    // w/o async await v
-    // fetch(url)
-    //   .then((res) => {
-    //     console.log(res);
-    //     return res.json();
-    //   })
-    //   .then(({ results }) => {
-    //     console.log(results);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
-  }
+  useEffect(() => {
+    loadQuestions()
+      .then((questions) => setQuestions(questions))
+      .catch((err) => console.err(err));
+  }, []);
 
-  scoreSaved = () => {
-    this.props.history.push('/');
-  }
+  // async componentDidMount() {
+  //   try {
+  //     const questions = await loadQuestions();
+  //     console.log(questions);
+  //     this.setState({ questions }, () => {
+  //       this.changeQuestion();
+  //     },
+  //     );
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // w/o async await v
+  // fetch(url)
+  //   .then((res) => {
+  //     console.log(res);
+  //     return res.json();
+  //   })
+  //   .then(({ results }) => {
+  //     console.log(results);
+  //   })
+  //   .catch((err) => {
+  //     console.error(err);
+  //   });
 
-  changeQuestion = (bonus = 0) => {
-    if (this.state.questions.length === 0) {
-      return this.setState((prevState) => ({
-        done: true,
-        score: prevState.score + bonus,
-      }));
-    }
-    // get a random index of a question
-    const randomQuestionIndex = Math.floor(Math.random() * this.state.questions.length);
-    // set the current question to the question at that random index
-    const currentQuestion = this.state.questions[randomQuestionIndex];
-    // remove that question from the questions going forward
-    const remainingQuestions = [...this.state.questions];
-    remainingQuestions.splice(randomQuestionIndex, 1);
-    // update the state to reflect these changes
-    this.setState((prevState) => ({
-      questions: remainingQuestions,
-      currentQuestion,
-      loading: false,
-      score: (prevState.score += bonus),
-      questionNumber: prevState.questionNumber + 1,
-    }));
+  const scoreSaved = () => {
+    history.push('/');
   };
 
-  render() {
-    const {
-      loading, done, score, currentQuestion, questionNumber,
-    } = this.state;
-    return (
-      <>
-        {loading && !done && <div id="loader" />}
-        {!done && !loading && currentQuestion && (
-          <div>
-            <HUD score={score} questionNumber={questionNumber} />
-            <Question question={currentQuestion} changeQuestion={this.changeQuestion} />
-          </div>
-        )}
-        {done && <SaveScoreForm score={score} scoreSaved={this.scoreSaved} />}
-      </>
-    );
-  }
+  const changeQuestion = useCallback((bonus = 0) => {
+    if (questions.length === 0) {
+      setDone(true);
+      return setScore(score + bonus);
+    }
+    // get a random index of a question
+    const randomQuestionIndex = Math.floor(Math.random() * questions.length);
+    // set the current question to the question at that random index
+    const currentQuestion = questions[randomQuestionIndex];
+    // remove that question from the questions going forward
+    const remainingQuestions = [...questions];
+    remainingQuestions.splice(randomQuestionIndex, 1);
+    // update the state to reflect these changes
+    setQuestions(remainingQuestions);
+    setCurrentQuestion(currentQuestion);
+    setLoading(false);
+    setScore(score + bonus);
+    setQuestionNumber(questionNumber + 1);
+  }, [score, questionNumber, questions, setQuestions, setLoading, setCurrentQuestion, setQuestionNumber]);
+
+  useEffect(() => {
+    if (!currentQuestion && questions.length) {
+      changeQuestion();
+    }
+  }, [currentQuestion, questions, changeQuestion]);
+
+  return (
+    <>
+      {loading && !done && <div id="loader" />}
+      {!done && !loading && currentQuestion && (
+      <div>
+        <HUD score={score} questionNumber={questionNumber} />
+        <Question question={currentQuestion} changeQuestion={changeQuestion} />
+      </div>
+      )}
+      {done && <SaveScoreForm score={score} scoreSaved={scoreSaved} />}
+    </>
+  );
 }
